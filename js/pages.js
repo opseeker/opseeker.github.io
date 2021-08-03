189b881417aff4381850a5047a02d5377055d5c3
opseeker <ankitsaikia19@gmail.com>
2021-08-03 08:00:46 +0530

    Create pages.js
    js for pages inside the site

---
+++ js/pages.js
@@ -0,0 +1,23 @@
+var menu=document.getElementById('menu-content');
+var nav=document.getElementById('navigation');
+
+
+function goBack(){
+    window.history.back();
+};
+
+function mode(){
+    document.body.classList.toggle('dark-mode');
+};
+
+function menuH(){
+    menu.classList.add('hide');
+};
+
+function menut(){
+    menu.classList.toggle('hide');
+};
+
+function navClose(){
+    
+};
