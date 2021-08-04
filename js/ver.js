84293bd02d749d6f493aecf6f9d1de5858687cd4
opseeker <ankitsaikia19@gmail.com>
2021-08-04 21:22:28 +0530

    Update ver.js

--- js/ver.js
+++ js/ver.js
@@ -1,4 +1,12 @@
 var num=document.getElementById('version');
+var target=document.getElementById('startup');
+
 onload=function() {
     num.innerHTML=('080421')
+    setTimeout(function(){target.classList.remove('hide');target.classList.add('startupOn')},4000);
 };
+
+function startCl(){
+    setTimeout(function(){target.classList.add('startupOff');},0);
+    setTimeout(function(){target.classList.add('hide');},4000);
+}
