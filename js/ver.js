64d78e6059f807f3ba84c591c041191d2477749f
opseeker <ankitsaikia19@gmail.com>
2021-08-05 08:01:06 +0530

    Update ver.js

--- js/ver.js
+++ js/ver.js
@@ -1,8 +1,11 @@
 var num=document.getElementById('version');
 var target=document.getElementById('startup');
-
+var load=document.getElementById('loader');
 onload=function() {
     num.innerHTML=('080521');
+    load.classList.remove('hide');
+    load.classList.add('login');
+    setTimeout(function(){load.classList.add('logout');load.classList.add('hide');},900);
     setTimeout(function(){target.classList.remove('hide');target.classList.add('startupOn')},4000);
 };
 
