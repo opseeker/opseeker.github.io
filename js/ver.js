fb9736aa5a69f135f0225f5a0603252077cd7ed9
opseeker <ankitsaikia19@gmail.com>
2021-08-05 12:33:58 +0530

    Update ver.js

--- js/ver.js
+++ js/ver.js
@@ -1,16 +1,16 @@
 var num=document.getElementById('version');
 var target=document.getElementById('startup');
 var load=document.getElementById('loader');
+
 onload=function() {
     num.innerHTML=('080521');
     load.classList.remove('hide');
-    setTimeout(function(){load.classList.add('logout');},2000);
-    setTimeout(function(){load.classList.add('hide');},3000);
-    setTimeout(function(){target.classList.remove('hide');target.classList.add('startupOn')},4000);
+    setTimeout(function(){load.classList.add('logout');},1000);
+    setTimeout(function(){target.classList.remove('hide');target.classList.add('startupOn')},1550);
+    setTimeout(function(){load.classList.add('hide');},2000);
 };
 
 function startCl(){
-    setTimeout(function(){target.classList.add('startupOff');},0);
-    setTimeout(function(){target.classList.add('hide');},4000);
-}
-
+    setTimeout(function(){load.classList.add('hide');target.classList.remove('startupOn');target.classList.add('startupOff');},0);
+    setTimeout(function(){target.classList.add('hide');target.classList.remove('startupOff');},500);
+};
