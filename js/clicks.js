e435baa60424ab51ccd0c44c81b2770ac8315680
opseeker <ankitsaikia19@gmail.com>
2021-08-13 18:46:13 +0530

    Update clicks.js

--- js/clicks.js
+++ js/clicks.js
@@ -2,13 +2,20 @@
 
 var menu=document.getElementById('menu-content');
 var menubtn=document.getElementById('menu-nav');
+var i1=document.getElementById('ico1');
+var i2=document.getElementById('ico2');
+
 
 function menuH(){
+    i1.classList.remove('hide');
+    i2.classList.add('hide');
     menu.classList.remove('menu-enter');
     menu.classList.add('menu-exit');
     setTimeout(function(){menu.classList.add('hide');},300);
 };
 function menut(){
+    i1.classList.add('hide');
+    i2.classList.remove('hide');
     menu.classList.remove('hide');
     menu.classList.add('menu-enter');
     menu.classList.remove('menu-exit');
