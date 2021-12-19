26f851d090cf328d0955f2222d68bcf94d3d9888
opseeker <ankitsaikia19@gmail.com>
2021-12-19 20:42:00 +0530

    upload knc helper site

---
+++ kncHelp/nav.js
@@ -0,0 +1,12 @@
+// self directory onload function..
+window.onload = function () {
+    var node = document.getElementById("_node");
+    node.innerHTML = '<ul><li class = "on" ><a href = "javascript:GoHome()"class = "nav-home" ><i> </i><span> Home </span></a></li><li><a href = "javascript:Device()" class = "nav-device" ><i></i><span> Machine </span></a></li><li><a href = "javascript:Wallet()" class = "nav-wallet" ><i></i><span> Wallet </span></a></li><li><a href = "javascript:Personal()" class = "nav-personal" ><i></i><span> Personal </span></a></li></ul>';
+}
+
+function gettingstarted () {
+    // var console=document.getElementById("_console");
+    // console.innerHTML='<p>alsa</p>';
+}
+
+
