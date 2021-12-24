f15fa03ece815936937a91848de982eb87389138
opseeker <ankitsaikia19@gmail.com>
2021-12-24 20:17:02 +0530

    update uwu

---
+++ assets/pages/js/click.js
@@ -0,0 +1,40 @@
+// click javascript
+var menu=document.getElementById('menu-content');
+var menubtn=document.getElementById('menu-nav');
+var i1=document.getElementById('ico1');
+var i2=document.getElementById('ico2');
+var s1=document.getElementById('vlink4');
+var hm=document.getElementById('_manhwa');
+var hmc=document.getElementById('_manhwa-c');
+var scr=document.getElementById('_mains');
+
+function manhwa() {
+    s1.classList.add('hide');
+    hm.classList.add('hide');
+    //hmc.classList.remove('hide');
+}
+
+function menuH(){
+    i1.classList.remove('hide');
+    i2.classList.add('hide');
+    menu.classList.remove('menu-enter');
+    menu.classList.add('menu-exit');
+    scr.classList.remove('hide');
+    scr.classList.remove('startupOff');
+    setTimeout(function(){menu.classList.add('hide');},300);
+};
+function menut(){
+    i1.classList.add('hide');
+    i2.classList.remove('hide');
+    scr.classList.add('startupOff');
+    setTimeout(function(){scr.classList.add('hide');},300);
+    menu.classList.remove('hide');
+    menu.classList.add('menu-enter');
+    menu.classList.remove('menu-exit');
+};
+
+function mode(){document.body.classList.toggle('dark-mode');}
+
+function goBack(){
+    window.history.back();
+};
---
+++ assets/pages/js/ver.js
@@ -0,0 +1,58 @@
+var num=document.getElementById('version');
+var target=document.getElementById('startup');
+var load=document.getElementById('loader');
+var ads=document.getElementById('ad001');
+var menu=document.getElementById('menu-content');
+
+//start
+window.onload=function() {
+    if(!localStorage.getItem('startMesseges')){
+        //load.classList.remove('hide');
+        setTimeout(function(){load.classList.add('logout');},1000);
+        //setTimeout(function(){target.classList.remove('hide');target.classList.add('startupOn')},1550);
+       // setTimeout(function(){load.classList.add('hide');},2000);
+        localStorage.setItem("startMesseges", "true");
+    }
+        //menu.classList.add('hide');
+        num.innerHTML=('081321'); 
+
+    //ads.classList.remove('hide');
+    //setTimeout(function(){ads.classList.add('enter-right');},2000);
+
+    var minutes = 10;
+    var now = new Date().getTime();
+    var setupTime = localStorage.getItem('setupTime');
+        if (setupTime == null) {
+        localStorage.setItem('setupTime', now)
+        } else {
+            if(now-setupTime > minutes*60*1000) {
+            //localStorage.removeItem('startMesseges');
+            localStorage.clear()
+            localStorage.setItem('setupTime', now);
+            }
+        } 
+    };
+
+function startCl(){
+    setTimeout(function(){load.classList.add('hide');target.classList.remove('startupOn');target.classList.add('startupOff');},0);
+    setTimeout(function(){target.classList.add('hide');target.classList.remove('startupOff');},500);
+};
+
+function closeAd(){
+    ads.classList.remove('enter-right');
+    ads.classList.add('exit-right');
+    setTimeout(function(){ads.classList.add('hide');ads.classList.remove('exit-right');},3000);
+}
+
+//localStorage.removeItem("name of localStorage variable you want to remove");
+
+function splay(){   //songpromotion
+    var mainaud=document.getElementById('controlthebeat');
+    var audio=document.getElementById('saud');
+    var soaud=document.getElementById('songp');
+    var prVid=document.getElementById('prV');
+    //audio.play();
+    prVid.pause();
+    mainaud.classList.add('startupOff');
+    setTimeout(function(){soaud.classList.add('hide');},500);
+}
