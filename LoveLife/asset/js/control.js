06a6abea86db5f5b5e97474761c612292f4f3b3f
opseeker <ankitsaikia19@gmail.com>
2021-12-25 21:37:37 +0530

    unique

--- LoveLife/asset/js/control.js
+++ LoveLife/asset/js/control.js
@@ -1,9 +1,18 @@
 function menuOpen(){
     var menuBtn=document.getElementById("_i1");
+    var body=document.getElementById("_mains");
+    var table=document.getElementById("_table1");
+    var gallery=document.getElementById("_gallery");
     if(menuBtn.classList.contains("on")){
         menuBtn.classList.remove("on");
+        body.classList.remove("hide");
+        table.classList.remove("hide");
+        gallery.classList.add("hide");
     }
     else{
         menuBtn.classList.add("on");
+        body.classList.add("hide");
+        table.classList.add("hide");
+        gallery.classList.remove("hide");
     }
 }
