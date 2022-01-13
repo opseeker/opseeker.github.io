64a05d8e4f95e1c4e68b3b9c2df1de447d69aeec
opseeker <ankitsaikia19@gmail.com>
2022-01-13 14:30:28 +0530

    up up

--- LoveLife/asset/js/scroll.js
+++ LoveLife/asset/js/scroll.js
@@ -8,8 +8,8 @@ function reveal() {
     var reveals = document.querySelectorAll(".reveal");
 
     for (var i = 0; i < reveals.length; i++) {
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 50;
+        var elementTop = reveals[i].getBoundingClientRect().top,
+        elementVisible = 50;
 
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
@@ -23,8 +23,8 @@ function goLeft() {
     var item=document.querySelectorAll(".goLeft");
 
     for (var i = 0; i < item.length; i++) {
-        var elementTop = item[i].getBoundingClientRect().top;
-        var elementVisible = 50;
+        var elementTop = item[i].getBoundingClientRect().top,
+        elementVisible = 50;
 
         if (elementTop < windowHeight - elementVisible) {
             item[i].classList.add("active");
@@ -39,8 +39,8 @@ function goRight() {
     var item=document.querySelectorAll(".goRight");
 
     for (var i = 0; i < item.length; i++) {
-        var elementTop = item[i].getBoundingClientRect().top;
-        var elementVisible = 50;
+        var elementTop = item[i].getBoundingClientRect().top,
+        elementVisible = 50;
 
         if (elementTop < windowHeight - elementVisible) {
             item[i].classList.add("active");
@@ -50,20 +50,24 @@ function goRight() {
         }
     }
 }
-    var pwd="pokor@123";var pwd1="Pokor@123";var pwd2="POKOR@123";
-    var pwd3="bobu@123";var pwd4="Bobu@123";var pwd9="whateverus";var pwd10="Whateverus";
-    var pwd5="anime@21";var pwd6="otaku@21";var pwd7="manisaikia";var pwd8="23112021";
+
+var pwd="pokor@123",pwd1="Pokor@123",pwd2="POKOR@123",
+pwd3="bobu@123",pwd4="Bobu@123",pwd9="whateverus",pwd10="Whateverus",
+pwd5="anime@21",pwd6="otaku@21",pwd7="manisaikia",pwd8="23112021";
+
 function loginSuccess(){
     var input=document.getElementById("t1").value;
     if(input==pwd || input==pwd1 || input==pwd2 || input==pwd3 || input==pwd4 || input==pwd5 || input==pwd6 || input==pwd7 || input==pwd8 || input==pwd9 || input==pwd10){
         document.getElementById("_body0").classList.remove("hide");
         document.getElementById("_passwordE").classList.add("hide");
         tabOnline();
+        adminCame();
     }
     else
-        void(0);   
+        error1();   
 }
 
+//table hidden conttent secure
 function tabOnline(){
     var input=document.getElementById("t1").value;
     if(input==pwd || input==pwd1 || input==pwd2 || input==pwd8){
@@ -71,5 +75,41 @@ function tabOnline(){
         document.getElementById("tab10").classList.remove("hide");
         document.getElementById("tab11").classList.remove("hide");
     }
+    else {
+        document.getElementById("tab8").classList.add("hide");
+        document.getElementById("tab10").classList.add("hide");
+        document.getElementById("tab11").classList.add("hide");
+    }
 }
 
+//empty password error
+function error1(){
+    var input=document.getElementById("t1").value,
+    a="",b=" ",c="   ",d="  ",e="        ";
+    if(input==a||input==b||input==c||input==d||input==e)
+    alert("Password is required to login");
+    else
+    error3();
+}
+
+//wrong password
+function error2() {
+    // var input=document.getElementById("t1").value;
+    alert("Invalid Password");
+}
+
+//at least 8 char
+function error3() {
+    var input=document.getElementById("t1").value;
+    if(input.length<8)
+    alert("Password must be at least 8 characters");
+    else
+    error2();
+}
+
+//admin entry
+function adminCame(){
+    var input=document.getElementById("t1").value;
+    if(input==pwd||input==pwd1||input==pwd2||input==pwd6)
+    alert("You have entered with the main password and all the contents are visible.");
+}
